4d9917703b51b507d550b573e74d7bf4b87f1129
Julien Bernard <artessbm@gmail.com>
2021-03-05 21:47:45 +0100

    More explicit ban logic variable names

--- include/logic.hpp
+++ include/logic.hpp
@@ -10,7 +10,7 @@ enum class ParasolDashing { BAN, DONT_BAN };
 enum class SlightSideB { BAN, DONT_BAN };
 
 /* Pick whether to apply optional B0XX/F1 bans */
-void initLogic(ParasolDashing banParasolDashing, SlightSideB banSlightSideB);
+void initLogic(ParasolDashing parasolDashingConfig, SlightSideB slightSideBConfig);
 
 /* Makes a B0XX/F1 report from the provided RectangleInput */
 GCReport makeReport(const RectangleInput &ri);
