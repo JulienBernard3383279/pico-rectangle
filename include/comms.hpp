511540818c780f4a2d9d2999faed9b532c2a1bbd
Julien Bernard <artessbm@gmail.com>
2021-05-26 19:11:56 +0200

    Fix dpad up and down being swapped in console mode

--- include/comms.hpp
+++ include/comms.hpp
@@ -5,7 +5,7 @@
 
 struct __attribute__((packed)) GCReport {
     uint8_t a : 1; uint8_t b : 1; uint8_t x:1; uint8_t y : 1; uint8_t start : 1; uint8_t pad0 : 3;
-    uint8_t dLeft : 1; uint8_t dRight : 1; uint8_t dUp : 1; uint8_t dDown : 1; uint8_t z : 1; uint8_t r : 1; uint8_t l : 1; uint8_t pad1 : 1;
+    uint8_t dLeft : 1; uint8_t dRight : 1; uint8_t dDown : 1; uint8_t dUp : 1; uint8_t z : 1; uint8_t r : 1; uint8_t l : 1; uint8_t pad1 : 1;
     uint8_t xStick;
     uint8_t yStick;
     uint8_t cxStick;
@@ -16,7 +16,7 @@ struct __attribute__((packed)) GCReport {
 
 const GCReport defaultGcReport = {
     .a=0, .b=0, .x=0, .y=0, .start=0, .pad0=0,
-    .dLeft=0, .dRight=0, .dUp=0, .dDown=0, .z=0, .r=0, .l=0, .pad1=1,
+    .dLeft=0, .dRight=0, .dDown=0, .dUp=0, .z=0, .r=0, .l=0, .pad1=1,
     .xStick=128,
     .yStick=128,
     .cxStick=128,
