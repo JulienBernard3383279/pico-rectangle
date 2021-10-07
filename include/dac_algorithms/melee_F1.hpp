e2d4d8c4bdf67a3319d77204002fb67f9a3ddc7c
Julien Bernard <artessbm@gmail.com>
2021-10-08 00:39:32 +0200

    Fix wrong include

--- include/dac_algorithms/melee_F1.hpp
+++ include/dac_algorithms/melee_F1.hpp
@@ -3,8 +3,6 @@
 
 #include "communication_protocols/joybus/gcReport.hpp"
 
-#include "usb_configurations/wired_fight_pad_pro.hpp"
-
 #include "gpio_to_button_sets/F1.hpp"
 
 namespace DACAlgorithms {
