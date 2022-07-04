e9d95162fcebbb93330b9e2adbd89e20157e4c6e
Julien Bernard <artessbm@gmail.com>
2022-07-04 23:01:16 +0200

    Make it easier to edit the keyboard mappings

--- src/dac_algorithms/set_of_8_keys.cpp
+++ src/dac_algorithms/set_of_8_keys.cpp
@@ -27,32 +27,38 @@ struct KeyMapping {
     char key;
 };
 
+
+// If you want to configure a key that isn't a lowercase letter of the alphabet, refer to
+// https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2 and replace the whole
+// "USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('q')" by the hex code that maps
+// to your key. For exemple, for enter on L: { &F1::ButtonSet::l,      0x28  }
+
 const KeyMapping keyMappings[] = {
-    { &F1::ButtonSet::l, 'q' },
-    { &F1::ButtonSet::left, 'w' },
-    { &F1::ButtonSet::down, 'e' },
-    { &F1::ButtonSet::right, 'r' },
-    { &F1::ButtonSet::mx, 'a' },
-    { &F1::ButtonSet::my, 's' },
-    { &F1::ButtonSet::start, 't' },
-    { &F1::ButtonSet::cDown, 'c' },
-    { &F1::ButtonSet::cLeft, 'f' },
-    { &F1::ButtonSet::cUp, 'g' },
-    { &F1::ButtonSet::a, 'v' },
-    { &F1::ButtonSet::cRight, 'b' },
-    { &F1::ButtonSet::r, 'y' },
-    { &F1::ButtonSet::b, 'h' },
-    { &F1::ButtonSet::y, 'u' },
-    { &F1::ButtonSet::x, 'j' },
-    { &F1::ButtonSet::ls, 'i' },
-    { &F1::ButtonSet::z, 'k' },
-    { &F1::ButtonSet::ms, 'o' },
-    { &F1::ButtonSet::up, 'l' }
+    { &F1::ButtonSet::l,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('q') },
+    { &F1::ButtonSet::left,   USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('w') },
+    { &F1::ButtonSet::down,   USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('e') },
+    { &F1::ButtonSet::right,  USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('r') },
+    { &F1::ButtonSet::mx,     USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('a') },
+    { &F1::ButtonSet::my,     USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('s') },
+    { &F1::ButtonSet::start,  USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('t') },
+    { &F1::ButtonSet::cDown,  USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('c') },
+    { &F1::ButtonSet::cLeft,  USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('f') },
+    { &F1::ButtonSet::cUp,    USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('g') },
+    { &F1::ButtonSet::a,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('v') },
+    { &F1::ButtonSet::cRight, USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('b') },
+    { &F1::ButtonSet::r,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('y') },
+    { &F1::ButtonSet::b,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('h') },
+    { &F1::ButtonSet::y,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('u') },
+    { &F1::ButtonSet::x,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('j') },
+    { &F1::ButtonSet::ls,     USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('i') },
+    { &F1::ButtonSet::z,      USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('k') },
+    { &F1::ButtonSet::ms,     USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('o') },
+    { &F1::ButtonSet::up,     USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter('l') }
 };
 
 void appendKey(char key, int& index) {
     if (index==8) return;
-    USBConfigurations::Keyboard8KRO::hidReport[index] = USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter(key);
+    USBConfigurations::Keyboard8KRO::hidReport[index] = key;
     index++;
 }
 
@@ -63,7 +69,7 @@ void actuate8KeysReport(F1::ButtonSet buttonSet) {
     for (KeyMapping keyMapping : keyMappings) {
         if (buttonSet.*(keyMapping.ptrToMember)) {
             gpio_put(25, 1);
-            log_uart0_int(USBConfigurations::Keyboard8KRO::keyIdFromLowerCaseLetter(keyMapping.key)); log_uart0(" ");
+            log_uart0_int(keyMapping.key); log_uart0(" ");
             appendKey(keyMapping.key, index);
         }
     }
